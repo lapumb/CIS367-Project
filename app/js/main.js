c236191cb7f0e90fbc352a8129412e77afc589b4
Blake Lapum <lapumb@mail.gvsu.edu>
2019-04-13 16:20:48 -0400

    trying to load a .obj. "THREE is not defined"error

--- app/js/main.js
+++ app/js/main.js
@@ -1,65 +1,147 @@
 import * as THREE from 'three';
 import TrackballControls from 'three-trackballcontrols';
-import Car from './models/Car.js'; 
+import Car from './models/Car.js';
 import Wheel from './models/Wheel.js';
+//import OBJLoader from 'three-obj-loader';
+//OBJLoader(THREE); 
+import './loaders/OBJLoader.js'
+
 
 export default class App {
-    constructor() {
-        const c = document.getElementById('mycanvas');
-        // Enable antialias for smoother lines
-        this.renderer = new THREE.WebGLRenderer({canvas: c, antialias: true});
-        this.scene = new THREE.Scene();
-        // Use perspective camera:
-        //   Field of view: 75 degrees
-        //   Screen aspect ration 4:3
-        //   Near plane at z=0.5, far plane at z=500
-        this.camera = new THREE.PerspectiveCamera(75, 4/3, 0.5, 500);
-        // Place the camera at (0,0,100)
-        this.camera.position.z = 100;
+  constructor() {
+    const c = document.getElementById('mycanvas');
+    // Enable antialias for smoother lines
+    this.renderer = new THREE.WebGLRenderer({ canvas: c, antialias: true });
+    this.scene = new THREE.Scene();
 
-        this.tracker = new TrackballControls(this.camera);
-        this.tracker.rotateSpeed = 2.0;
-        // Allow zoom and pan
-        this.tracker.noZoom = false;
-        this.tracker.noPan = false;
+    // Use perspective camera:
+    //   Field of view: 75 degrees
+    //   Screen aspect ration 4:3
+    //   Near plane at z=0.5, far plane at z=500
+    this.camera = new THREE.PerspectiveCamera(75, 4 / 3, 0.5, 500);
+    // Place the camera at (0,0,100)
+    this.camera.position.z = 100;
 
-        //adding light so I can see what I am doing
-        const lightOne = new THREE.DirectionalLight (0xFFFFFF, 1.0);
-        lightOne.position.set (10, 40, 100);
-        this.scene.add (lightOne);
+    this.tracker = new TrackballControls(this.camera);
+    this.tracker.rotateSpeed = 2.0;
+    // Allow zoom and pan
+    this.tracker.noZoom = false;
+    this.tracker.noPan = false;
 
-        const lightTwo = new THREE.DirectionalLight(0xFFFFFF, 1.0); 
-        lightTwo.position.set(-10, -40, -100); 
-        this.scene.add(lightTwo);
+    const keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+    keyLight.position.set(-100, 0, 100);
 
-        this.createCar(); 
-    
-        window.addEventListener('resize', () => this.resizeHandler());
-        this.resizeHandler();
-        requestAnimationFrame(() => this.render());
+    const fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+    fillLight.position.set(100, 0, 100);
+
+    const backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+    backLight.position.set(100, 0, -100).normalize();
+
+    this.scene.add(keyLight);
+    this.scene.add(fillLight);
+    this.scene.add(backLight);
+
+    //adding light so I can see what I am doing
+    /*const lightOne = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+    lightOne.position.set(10, 40, 100);
+    this.scene.add(lightOne);
+
+    const lightTwo = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+    lightTwo.position.set(-10, -40, -100);
+    this.scene.add(lightTwo);
+
+    //loading in the deer, possibly
+    // instantiate a loader
+    /*var loader = new THREE.OBJLoader();
+
+    // load a resource
+    loader.load(
+      // resource URL
+      'models/Deer.obj',
+      // called when resource is loaded
+      function (object) {
+
+        scene.add(object);
+
+      },
+      // called when loading is in progresses
+      function (xhr) {
+
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+      },
+      // called when loading has errors
+      function (error) {
+
+        console.log('An error happened');
+
       }
+    ); */
     
-      render() {
-        this.renderer.render(this.scene, this.camera);
-        this.tracker.update();
+    this.loader = new OBJLoader(); 
+    this.loader.setPath('/app/js/models/');
+    this.loader.load('Deer.obj', function(object){
+        object.position.y -= 60; 
+        this.scene.add(object); 
+    }); 
 
-        requestAnimationFrame(() => this.render());
+    //this.createCar();
+
+    this.rotZ1 = new THREE.Matrix4().makeRotationZ(THREE.Math.degToRad(1));
+
+    window.addEventListener('resize', () => this.resizeHandler());
+    this.resizeHandler();
+    requestAnimationFrame(() => this.render());
+  }
+
+  render() {
+    this.renderer.render(this.scene, this.camera);
+    this.tracker.update();
+
+    document.onkeydown = function(event) {
+      switch (event.keyCode) {
+         case 37:
+              alert('Left key pressed');
+            break;
+         case 38:
+              alert('Up key pressed');
+            break;
+         case 39:
+              alert('Right key pressed');
+              //this.fltire.translateX(20); 
+            break;
+         case 40:
+              alert('Down key pressed');
+            break;
+         default:
+            break; 
       }
-    
-      resizeHandler() {
-        const canvas = document.getElementById("mycanvas");
-        let w = window.innerWidth - 16;
-        let h = 0.75 * w;  /* maintain 4:3 ratio */
-        if (canvas.offsetTop + h > window.innerHeight) {
-          h = window.innerHeight - canvas.offsetTop - 16;
-          w = 4/3 * h;
-        }
-        canvas.width = w;
-        canvas.height = h;
-        this.camera.updateProjectionMatrix();
-        this.renderer.setSize(w, h);
-        this.tracker.handleResize();
-      }
+    };
+
+    //making tires spin
+    /*
+    this.bltire.matrix.multiply (this.rotZ1);
+    this.brtire.matrix.multiply (this.rotZ1);
+    this.frtire.matrix.multiply (this.rotZ1);
+    this.fltire.matrix.multiply (this.rotZ1); */
+
+    requestAnimationFrame(() => this.render());
+  }
+
+  resizeHandler() {
+    const canvas = document.getElementById("mycanvas");
+    let w = window.innerWidth - 16;
+    let h = 0.75 * w;  /* maintain 4:3 ratio */
+    if (canvas.offsetTop + h > window.innerHeight) {
+      h = window.innerHeight - canvas.offsetTop - 16;
+      w = 4 / 3 * h;
+    }
+    canvas.width = w;
+    canvas.height = h;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(w, h);
+    this.tracker.handleResize();
+  }
 
   createCar() {
     //adding body
@@ -73,6 +155,7 @@ export default class App {
     this.frtire.translateY(-5);
     this.frtire.translateZ(4);
     this.scene.add(this.frtire);
+    //this.frtire.matrixAutoUpdate = false;
 
     //front left tire
     this.fltire = new Wheel(8);
@@ -80,19 +163,22 @@ export default class App {
     this.fltire.translateY(-5);
     this.fltire.translateZ(-4);
     this.scene.add(this.fltire);
+    //this.fltire.matrixAutoUpdate = false;
 
     //back right tire
-    this.brtire = new Wheel(8); 
+    this.brtire = new Wheel(8);
     this.brtire.translateX(-3);
     this.brtire.translateY(-5);
     this.brtire.translateZ(-4);
     this.scene.add(this.brtire);
+    //this.brtire.matrixAutoUpdate = false;
 
     //back right tire
-    this.bltire = new Wheel(8); 
+    this.bltire = new Wheel(8);
     this.bltire.translateX(-3);
     this.bltire.translateY(-5);
     this.bltire.translateZ(4);
     this.scene.add(this.bltire);
+    //this.bltire.matrixAutoUpdate = false;
   }
 }
--- app/js/models/Car.js
+++ app/js/models/Car.js
@@ -23,31 +23,5 @@ export default class Car extends Group {
         hood.translateX(CAR_DEBTH);
         hood.translateY(-(CAR_HEIGHT/4));
         this.add(hood);
-
-        //wheels
-        /*
-        const WHEEL_RADIUS = 2;
-        const TIRE_THICKNESS = 1;
-        const tireGeo = new TorusGeometry(WHEEL_RADIUS, TIRE_THICKNESS, 6, 30);
-        const tireMat = new MeshPhongMaterial({ color: 0x82332a });
-        const frtire = new Mesh(tireGeo, tireMat);
-
-        frtire.translateY(-(CAR_DEBTH/2)); 
-        frtire.translateX(CAR_HEIGHT); 
-        frtire.translateZ(CAR_WIDTH/2); 
-        this.add(frtire);  // place the torus in the group
-
-        const numSpokes = 8; 
-        for (let k = 0; k < 6; k++) {
-            const spGeo = new CylinderGeometry(0.7 * TIRE_THICKNESS, 0.7 * TIRE_THICKNESS,
-                WHEEL_RADIUS, 10, 10);
-            const spMat = new MeshPhongMaterial({ color: 0x001199 });
-            const sp = new Mesh(spGeo, spMat);
-            sp.rotateZ(k * 2 * Math.PI / numSpokes);
-            sp.translateY(WHEEL_RADIUS / 2);
-            this.add(sp);   // place the cylinder in the group
-        }*/
-        // default to "return this;"
-
     }
 }
